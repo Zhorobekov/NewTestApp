79c829781aaf71e3e423bd68e4a921c0d42e684c
Zhorobekov <zhorobekov25@icloud.com>
2022-06-12 12:27:13 +0600

    Initial Commit

---
+++ NewTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewTestApp
+//
+//  Created by Эрмек Жоробеков on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

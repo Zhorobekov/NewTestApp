bc0a82960f21bc11959d4627e29bd224e8be10be
Azamat Satybaldiev <satibaldiev.a@inbox.ru>
2022-06-12 16:43:33 +0600

    add 2 view

---
+++ NewTestApp/ViewControllers/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  NewTestApp
+//
+//  Created by Azamat on 12.06.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ NewTestApp/ViewControllers/FavoriteViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteViewController.swift
+//  NewTestApp
+//
+//  Created by Azamat on 12.06.2022.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
